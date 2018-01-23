97240802d57a6b6f53c5c1b4fd46cb4a5630735c
Fernando Renteria <rentfern@gmail.com>
2018-01-23 16:18:08 -0600

    Added Logger4J

--- src/controller/ControllerTest.java
+++ src/controller/ControllerTest.java
@@ -1,5 +1,14 @@
 package controller;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ControllerTest {
-
+//	public static void main(String[] args) {
+//		//won't show because of the default logger config
+//		Logger logger = LogManager.getLogger(ControllerTest.class);
+//		logger.debug("This is a debug log message");
+//		
+//		//but this will
+//		logger.error("This is an error log message");
+//	}
 }
