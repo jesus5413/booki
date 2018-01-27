a014bf2c74862ac0b83fced4f20245a79ae9d35f
Fernando Renteria <rentfern@gmail.com>
2018-01-27 15:44:36 -0600

    Added dummy data and changed tableview to gridview

--- src/controller/AuthorDetailViewController.java
+++ src/controller/AuthorDetailViewController.java
@@ -3,19 +3,17 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
+import javafx.scene.layout.GridPane;
 
 public class AuthorDetailViewController implements Initializable {
+    @FXML private GridPane authorGrid;
     @FXML private Button saveButton;
-    @FXML private TableColumn<?, ?> firstName;
-    @FXML private TableColumn<?, ?> lastName;
-    @FXML private TableColumn<?, ?> doB;
-    @FXML private TableColumn<?, ?> gender;
-    @FXML private TableColumn<?, ?> website;
-
+    
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
 		
@@ -24,10 +22,11 @@ public class AuthorDetailViewController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		System.out.println("you clicked save button");
-	}
-	
-	public void openAuthorDetails() {
-		
+        saveButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
 	}
 }
