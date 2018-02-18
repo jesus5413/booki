b67c7140e98763657cdb0890127802d39ee75c2b
Fernando Renteria <rentfern@gmail.com>
2018-02-17 20:26:46 -0600

    See prev (oops)

---
+++ src/exception/AppException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class AppException extends RuntimeException{
+	public AppException(Exception e) {
+		super(e);
+	}
+	
+	public AppException(String msg) {
+		super(msg);
+	}
+}
