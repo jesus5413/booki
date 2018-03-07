94db9be8295182853abd68462442b5f236052d49
Fernando Renteria <rentfern@gmail.com>
2018-03-07 16:19:10 -0600

    Update BookTableGateway to handle book table calls

--- src/dataBase/BookTableGateWay.java
+++ src/dataBase/BookTableGateWay.java
@@ -9,8 +9,10 @@ import java.sql.SQLException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import exception.AppException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import model.AuthorModel;
 import model.BookModel;
 
 public class BookTableGateWay {
@@ -30,7 +32,6 @@ public class BookTableGateWay {
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://easel2.fulgentcorp.com:3306/wwh124?useSSL=false","wwh124","sanAntonio1234");
 			logger.debug("connection success\n");
-			//conn.close();
 		}catch (Exception e) {
 			logger.debug("some error\n");
 			logger.debug(e);
@@ -56,17 +57,79 @@ public class BookTableGateWay {
 			myStmt = conn.prepareStatement("select * from book");
 			rs = myStmt.executeQuery();
 			
+			// we'll make a new model and add it onto our list
+			BookModel book = new BookModel();
 			
+			book.setId(rs.getInt("ID"));
+			book.setTitle(rs.getString("title"));
+			book.setSummary(rs.getString("summary"));
+			book.setYearPublished(rs.getInt("year_published"));
+			//book.setPublisher(rs.getString("gender"));
+			book.setIsbn(rs.getString("isbn"));
+			//book.setDateAdded(rs.getDate("date_Added"));
+			bookList.add(book);
 			
+			logger.debug(book.getTitle());
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 		
 		return bookList;
-		
 	}
 	
-
+	public void deleteBook(int ID) {
+		try {
+			myStmt = conn.prepareStatement("delete from book where ID = ?");
+			myStmt.setInt(1, ID);
+			myStmt.execute();
+			logger.debug("deletion successful \n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void updateBook(BookModel book) {
+		try {
+			myStmt = conn.prepareStatement("update authorDetail set first_name = ? , last_name = ? , dob = ? , gender = ? , web_site = ? where ID = ?");
+			myStmt.setString(1, book.getTitle());
+			myStmt.setString(2, book.getSummary());
+			myStmt.setInt(3, book.getYearPublished());
+			//myStmt.setString(4, book.getPublisher());
+			myStmt.setString(5, book.getIsbn());
+			//myStmt.setInt(6, book.getDateAdded());
+			myStmt.executeUpdate();
+			logger.debug("update successful\n");
+		} catch (SQLException e) {
+			throw new AppException(e);
+		}	
+	}
+	
+	public void readBook(BookModel book) {
+		try {
+			// IDs are unique, thus we select the record where it matches
+			myStmt = conn.prepareStatement("select * from book where ID = ?");
+			myStmt.setInt(1, book.getId());
+			myStmt.executeQuery();
+			logger.debug("read successful\n");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveBook(BookModel book) {
+		try {
+			String query = " insert into authorDetail (first_name, last_name, dob, gender, web_site) values (? , ? , ? , ? , ?)";
+			myStmt = conn.prepareStatement(query);
+			myStmt.setString(1, book.getTitle());
+			myStmt.setString(2, book.getSummary());
+			myStmt.setInt(3, book.getYearPublished());
+			//myStmt.setString(4, book.getPublisher());
+			myStmt.setString(5, book.getIsbn());
+			myStmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
 
