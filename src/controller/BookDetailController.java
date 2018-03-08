d3f3b6b80866cde126faf17ba91434371f8a7936
jesus5413 <wwh124@my.utsa.edu>
2018-03-07 18:05:07 -0600

    gui complete

--- src/controller/BookDetailController.java
+++ src/controller/BookDetailController.java
@@ -1,5 +1,42 @@
 package controller;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import dataBase.TempStorage;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class BookDetailController {
+	@FXML
+	public Label ID;
+	public TextField title;
+	public TextArea summary;
+	public DatePicker yearPublished;
+	public ComboBox<String> publisher;
+	public TextField ISBN;
+	public Label dateAdded;
+	public Button update;
+	public Button save;
+	
+	public void initialize() {
+		if(TempStorage.oneBook != null) {
+			save.setVisible(false);
+			
+
+			
+		}else {
+			save.setVisible(true);
+			ID.setText("");
+			dateAdded.setText(java.time.LocalDate.now().toString());
+			update.setVisible(false);
+			
+		}
+		
+	}
+	
+	public void textFieldPlaceHolder() {
+		update.setVisible(true);
+	}
 
 }
