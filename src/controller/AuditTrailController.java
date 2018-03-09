cd97e4c1912a4ef56b080a357271d41daf67f3c8
jesus5413 <wwh124@my.utsa.edu>
2018-03-08 21:04:03 -0600

    finally

--- src/controller/AuditTrailController.java
+++ src/controller/AuditTrailController.java
@@ -2,47 +2,48 @@ package controller;
 
 import javax.swing.LayoutFocusTraversalPolicy;
 
-<<<<<<< HEAD
+import dataBase.BookTableGateWay;
 import dataBase.TempStorage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.AuditTrailModel;
 
 public class AuditTrailController {
 	ObservableList<AuditTrailModel> auditList = FXCollections.observableArrayList();
 	@FXML TableView<AuditTrailModel> auditTable;
-	@FXML TableColumn<AuditTrailModel, Integer> ID;
 	@FXML TableColumn<AuditTrailModel, String> dateAdded;
 	@FXML TableColumn<AuditTrailModel, String> message;
+	@FXML public Label labelAudit;
 	private int id;
 	
 	public void initialize(){
-		id = TempStorage.oneAuthor.getID();
-		TempStorage.oneAuthor = null;
+		id = TempStorage.oneBook.getId();
+		labelAudit.setText("Audit trail for " + TempStorage.oneBook.getTitle());
+		TempStorage.oneBook = null;
+		populateTable();
 	}
-	
-	
-=======
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import model.AuditTrailModel;
-
-public class AuditTrailController {
-	ObservableList<AuditTrailModel> auditList = FXCollections.observableArrayList();
-	@FXML TableView<AuditTrailModel> auditTable;
-	@FXML TableColumn<AuditTrailModel, Integer> ID;
-	@FXML TableColumn<AuditTrailModel, String> dateAdded;
-	@FXML TableColumn<AuditTrailModel, String> message;
-	
-	public void initialize(){
+	public void populateTable() {
+		BookTableGateWay bookCon = new BookTableGateWay();
+		bookCon.setConnection();
+		auditList = bookCon.getAuditTrail(id); 
+		bookCon.closeConnection();
+		
+		setCell();
+		auditTable.setItems(auditList);
 		
 	}
->>>>>>> branch 'fern1' of https://github.com/jesus5413/booki.git
-
+	
+	public void setCell() {
+		
+		dateAdded.setCellValueFactory(new PropertyValueFactory<>("dateAdded"));
+		message.setCellValueFactory(new PropertyValueFactory<>("msg"));
+		
+	}
+	
+	
 }
